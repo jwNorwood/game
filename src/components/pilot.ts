09f077e6c1aec45bc9691bd2574983691f57b85a
Jacob Norwood <jacobnorwood92@gmail.com>
2021-05-29 17:27:12 -0500

    formatting

--- src/components/pilot.ts
+++ src/components/pilot.ts
@@ -1,20 +1,28 @@
 
 export default class Pilot {
+  private currentHealth: integer;
+  private maxHealth: integer;
+
   constructor(maxHealth: integer, currentHealth: integer) {
     this.maxHealth = maxHealth;
     this.currentHealth = currentHealth;
   }
   // specialization(s)
   // deficiencies(s)
-  currentHealth: integer;
-  maxHealth: integer;
 
   updateMaxHealth( maxHealth: integer ){
     this.maxHealth = maxHealth
   }
+  getMaxHealth(){
+    return this.maxHealth
+  }
 
   updateCurrentHealth( currentHealth: integer ){
     this.currentHealth = currentHealth;
+  }
+  
+  getCurrentHealth(){
+    return this.currentHealth;
   }
 }
 
--- src/components/player.ts
+++ src/components/player.ts
@@ -2,40 +2,76 @@ import Deck from "./deck";
 import Body from "./body";
 
 export default class Player {
+  private currentHealth: integer;
+  private maxHealth: integer;
+  // current
+  private cash: integer;
+  // meta
+  private credits: integer;
+  
+  private model: string;
+  private heat: integer;
+
+  public deck: Class;
+  public body: Class;
+  
   constructor(
     maxHealth: integer,
     currentHealth: integer,
-    deck: Array<object>,
-    body: any,
     cash: integer,
     credits: integer,
-    model: string
+    model: string,
+    heat: integer,
+    deck: Array<object>,
+    body: any,
   ) {
     this.maxHealth = maxHealth;
     this.currentHealth = currentHealth;
-    this.model = model;
-    this.cash  = cash;
+    this.cash = cash;
     this.credits = credits;
+    this.model = model;
+    this.heat = heat;
+    
     this.deck = new Deck(deck);
     this.body = new Body(body);
   }
 
-  model: string;
-  currentHealth: integer;
-  maxHealth: integer;
-  deck: Class;
-  body: Class;
-
-  // current
-  cash: integer;
-  // meta
-  credits: integer;
-
   updateMaxHealth(maxHealth: integer) {
     this.maxHealth = maxHealth;
+  }
+  getMaxHealth() {
+    return this.maxHealth;
   }
 
   updateCurrentHealth(currentHealth: integer) {
     this.currentHealth = currentHealth;
+  }
+  getCurrentHealth() {
+    return this.currentHealth;
+  }
+
+  updateHeat(heat: integer) {
+    this.heat = heat;
+  }
+  getHeat() {
+    return this.heat;
+  }
+
+  updateCash(cash: integer) {
+    this.cash = cash;
+  }
+  getCash() {
+    return this.cash;
+  }
+
+  updateCredits(credits: integer) {
+    this.credits = credits;
+  }
+  getCredits() {
+    return this.credits;
+  }
+
+  getModel() {
+    return this.model;
   }
 }
