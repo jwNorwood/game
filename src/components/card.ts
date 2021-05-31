f206d650b44a33efcdc8acf7c83e3543623ea1e3
Jacob Norwood <jacobnorwood92@gmail.com>
2021-05-30 22:05:52 -0500

    card

--- src/components/card.ts
+++ src/components/card.ts
@@ -1,7 +1,52 @@
-// cost
-// name
-// id
-// text
-// rarity
+interface CardInterface {
+  id: integer;
+  name: string;
+  description: string;
+  actionCost: integer;
+  heatCost: integer;
+  rarity: integer;
 
-// some ablity system
+  onPlay: Function;
+  onDraw: Function;
+  onShuffle: Function;
+  onConsume: Function;
+}
+
+export default class Card {
+  public name: string;
+  public description: string;
+  public actionCost: integer;
+  public heatCost: integer;
+  public rarity: integer;
+
+  public onPlay: Function;
+  public onDraw: Function;
+  public onShuffle: Function;
+  public onConsume: Function;
+
+  private id: integer;
+
+  constructor({
+    name,
+    description,
+    actionCost,
+    heatCost,
+    id,
+    rarity,
+    onPlay,
+    onDraw,
+    onShuffle,
+    onConsume,
+  }: CardInterface) {
+    this.name = name;
+    this.description = description;
+    this.actionCost = actionCost;
+    this.heatCost = heatCost;
+    this.id = id;
+    this.rarity = rarity;
+    this.onPlay = onPlay;
+    this.onShuffle = onShuffle;
+    this.onDraw = onDraw;
+    this.onConsume = onConsume;
+  }
+}
